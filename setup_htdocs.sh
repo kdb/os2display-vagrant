f312c3ade284c89d4eef9cddd9a3326d3dbf155a
Mads H. Danquah <mads@danquah.dk>
2017-06-14 14:09:09 +0200

    Switching to screen from the kdb repo

--- setup_htdocs.sh
+++ setup_htdocs.sh
@@ -3,6 +3,6 @@ cd htdocs
 git clone git@github.com:kdb/os2display-admin.git admin
 git clone git@github.com:search-node/search_node.git search_node
 git clone git@github.com:os2display/middleware.git middleware
-git clone git@github.com:os2display/screen.git screen
+git clone git@github.com:kdb/os2display-screen.git screen
 git clone git@github.com:os2display/styleguide.git styleguide
 cd ..

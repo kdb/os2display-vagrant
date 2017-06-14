cd4033fff8ebfc529700366a1769c647bf024aa2
Mads H. Danquah <mads@danquah.dk>
2017-06-14 08:10:26 +0200

    Switching to KDBs fork of admin

--- setup_htdocs.sh
+++ setup_htdocs.sh
@@ -1,6 +1,6 @@
 mkdir htdocs
 cd htdocs
-git clone git@github.com:os2display/admin.git admin
+git clone git@github.com:kdb/os2display-admin.git admin
 git clone git@github.com:search-node/search_node.git search_node
 git clone git@github.com:os2display/middleware.git middleware
 git clone git@github.com:os2display/screen.git screen

2b330e831f5b8a56a4212e65c0d360667478bb4b
Mads H. Danquah <mads@danquah.dk>
2018-08-03 15:44:49 +0200

    Switch to stable fork branch for admin
    OS2-89

--- scripts/setup_htdocs.sh
+++ scripts/setup_htdocs.sh
@@ -7,7 +7,7 @@ cd $dir/htdocs
 git clone git@github.com:search-node/search_node.git search_node
 
 git clone git@github.com:os2display/docs.git docs
-git clone -b feature/upgrade-v5 git@github.com:kdb/os2display-admin.git admin
+git clone -b kkbdisplay git@github.com:kdb/os2display-admin.git admin
 git clone git@github.com:os2display/middleware.git middleware
 git clone git@github.com:os2display/screen.git screen
 
